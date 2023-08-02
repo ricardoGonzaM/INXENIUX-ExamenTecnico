964ba487991b7789d06e8bd57113fd4d28f1642b
Sierra117 <ricardogonzalezmendez117@hotmail,com>
2023-08-02 00:13:31 -0600

    deployment

--- INXENIUX-ExamenTecnico/node/database/db.js
+++ INXENIUX-ExamenTecnico/node/database/db.js
@@ -1,13 +1,14 @@
 //Conexion de la base de datos
 import { Sequelize } from "sequelize";
 
-const  db = new Sequelize('database_app', 'root' , 'manager', {
-        host: 'localhost',
-        dialect: 'mysql',
-        define: {
-                timestamps: false
-            }
-
+const  db = new Sequelize({
+        host: process.env.DB_HOST, 
+        user: process.env.DB_USERNAME, 
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DBNAME,
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0
 })
 
 export default db
