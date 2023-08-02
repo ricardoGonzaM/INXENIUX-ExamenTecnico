83f3141cea79810df75e6d72dd04fe1e01302d98
Sierra117 <ricardogonzalezmendez117@hotmail,com>
2023-08-02 14:58:02 -0600

    deployment

--- INXENIUX-ExamenTecnico/node/app.js
+++ INXENIUX-ExamenTecnico/node/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 app.use("/blogs", blogRoutes);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log("Server Up running ");
