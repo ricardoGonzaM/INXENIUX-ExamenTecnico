06efb18d6f3ab68f59ce2a2ad9fbea890cf68651
Sierra117 <ricardogonzalezmendez117@hotmail,com>
2023-08-03 00:11:38 -0600

    deployment

--- INXENIUX-ExamenTecnico/node/index.js
+++ INXENIUX-ExamenTecnico/node/index.js
@@ -1,9 +1,15 @@
+import blogRoutes from "./routes/routes.js";
+import cors from "cors";
+
 const express = require("express");
 const app = express();
-
 const port = process.env.PORT || 4000;
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
 app.listen(port);
+app.use("/blogs", blogRoutes);
 
 app.get("/", (req, res)=>{
   res.send("La pagina de inicio");
