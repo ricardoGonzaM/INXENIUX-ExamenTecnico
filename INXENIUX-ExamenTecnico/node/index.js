6a76a157efcb24c1552ba378c5a3a7223722313b
Sierra117 <ricardogonzalezmendez117@hotmail,com>
2023-08-03 01:43:38 -0600

    deployment

--- INXENIUX-ExamenTecnico/node/index.js
+++ INXENIUX-ExamenTecnico/node/index.js
@@ -1,6 +1,5 @@
-//import blogRoutes from "./routes/routes.js";
+import blogRoutes from "./routes/routes.js";
 import cors from "cors";
-import dba from "./database/db.js";
 
 
 const express = require("express");
@@ -11,15 +10,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 app.listen(port);
-//app.use("/blogs", blogRoutes);
+app.use("/blogs", blogRoutes);
 
 app.get("/", (req, res)=>{
   res.send("La pagina de inicio");
 });
-
-dba.getConnection((err, conn) => {
-  if(err) console.log(err)
-  console.log("Connected successfully")
-})
 
 console.log(`Listen on Port ${port}`);
