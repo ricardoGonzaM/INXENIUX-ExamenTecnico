7c5b307cd40a74f97ba204728bb7a266d475645f
Sierra117 <ricardogonzalezmendez117@hotmail,com>
2023-08-04 00:47:25 -0600

    deployment

--- INXENIUX-ExamenTecnico/my-app/src/blog/EditBlog.js
+++ INXENIUX-ExamenTecnico/my-app/src/blog/EditBlog.js
@@ -139,6 +139,7 @@ const CompEditBlog = ({ id }) => {
 
   const getpartById = async () => {
     const res = await axios.get("https://inxeniux-examen-tecnico-zfwa.vercel.app/blogs/part/" + newId);
+    console.log(res.data.data[0].I_pers)
     setI_pers(res.data.data[0].I_pers);
     setD_Pref(res.data.data[0].D_Pref);
     setT_Hab(res.data.data[0].T_Hab);
